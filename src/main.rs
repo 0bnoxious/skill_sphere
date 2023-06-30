2c06593ecaa8ef5d5911a6f165cf773089381362
0bnoxious <n.beaudet234@gmail.com>
2023-06-29 20:20:21 -0400

    Added world inspector

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,66 @@
-use std::f32::consts::PI;
-
 use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
-    render::render_resource::{Extent3d, TextureDimension, TextureFormat},
+    window::{CursorGrabMode, PresentMode, WindowLevel},
 };
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+pub const HEIGHT: f32 = 1080.0;
+pub const WIDTH: f32 = 1920.0;
 
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins)
-    .add_startup_system(setup)
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Skill Sphere".into(),
+            resolution: (WIDTH, HEIGHT).into(),
+            present_mode: PresentMode::AutoVsync,
+            // Tells wasm to resize the window according to the available canvas
+            fit_canvas_to_parent: true,
+            // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+            prevent_default_event_handling: false,
+            ..default()
+        }),
+        ..default()
+    }))
+    .add_startup_system(setup_scene)
+    .add_startup_system(setup_camera)
+    .add_plugin(WorldInspectorPlugin::new())
     .run();
 }
 
 #[derive(Component)]
-struct Sphere;
+struct SkillSphere;
+
+/*#[derive(Bundle)]
+struct SpherePole{
+    origin: SkillSphere,
+    skillname: str,
+    location: Vec3,
+
+}*/
+
+#[derive(Component)]
+struct SphereCamera{
+    /// The "focus point" to orbit around. It is automatically updated when panning the camera
+    pub focus: Vec3,
+    pub radius: f32,
+    pub upside_down: bool,
+}
 
 /// set up a simple 3D scene
-fn setup(
+fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
-    // sphere
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Icosphere::default().try_into().unwrap()),
         material: materials.add(Color::rgb(1.0, 0.1, 0.1).into()),
-        transform: Transform::from_xyz(0.0, 2.0, 0.0),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
         ..default()
-    });
-    // light
+    }).insert(Name::new("Sphere"));
+
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 1500.0,
@@ -43,10 +69,16 @@ fn setup(
         },
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
-    });
-    // camera
+    }).insert(Name::new("Light"));
+    
+}
+
+fn setup_camera(
+    mut commands: Commands,
+) {
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0)
+        .looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 }
