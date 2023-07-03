b1d0d15945e992b8f649b235f3409d21030b3f3b
0bnoxious <n.beaudet234@gmail.com>
2023-07-03 11:11:15 -0400

    Added skybox

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bevy::{
     window::{PresentMode},
 };
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_atmosphere::prelude::*;
 
 pub const HEIGHT: f32 = 1080.0;
 pub const WIDTH: f32 = 1920.0;
@@ -22,8 +23,7 @@ fn main() {
         }),
         ..default()
     }))
-    .register_type::<Pole>()
-    .add_startup_system(setup_asset)
+    .add_plugin(AtmospherePlugin)
     .add_startup_system(setup_scene)
     .add_startup_system(setup_camera)
     .add_plugin(WorldInspectorPlugin::new())
@@ -32,27 +32,39 @@ fn main() {
 
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
+pub struct SkillSphere {
+
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
 pub struct Pole {
     skillname: String,
-    locaation: Vec3,
 }
 
 fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
-    commands.spawn(SceneBundle {
+    let skillsphere = commands.spawn(SceneBundle {
+        scene: asset_server.load("Star4.glb#Scene0"),
         transform: Transform::from_xyz(0.0, 0.0, 0.0),
-        ..default()
-    }).insert(Name::new("Sphere"));
+        ..Default::default()
+    }).insert(Name::new("Sphere")).id();
 
-    commands.spawn(PbrBundle {
+    let pole = commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 0.5 })),
-        material: materials.add(Color::rgb(0.0, 0.7, 0.0).into()),
+        material: materials.add(Color::rgb(0.7, 0.0, 0.0).into()),
         transform: Transform::from_xyz(0.0, 1.0, 0.0),
         ..default()
-    }).insert(Name::new("Pole"));
+    }).insert(Pole{
+        skillname: "Fire".to_string(), 
+    }).insert(Name::new("Pole")).id();
+
+    // add the child to the parent
+    commands.entity(skillsphere).push_children(&[pole]);
 
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -63,28 +75,23 @@ fn setup_scene(
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
     }).insert(Name::new("Light"));
+
+    commands.insert_resource(AtmosphereModel::new(Nishita {
+        rayleigh_coefficient: Vec3::new(2e-5, 1e-5, 2e-5),
+        ..default()
+    }));
     
 }
 
 fn setup_camera(
     mut commands: Commands,
 ) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0)
-        .looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
-}
-
-fn setup_asset(
-    mut commands: Commands, 
-    asset_server: Res<AssetServer>
-) {
-    let sphere_asset = asset_server.load("Star.glb#Scene0");
-
-    commands.spawn(SceneBundle {
-        scene: sphere_asset,
-        transform: Transform::from_xyz(0.0, 0.0, 0.0),
-        ..Default::default()
-    });
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(-2.0, 1.0, 5.0)
+            .looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        AtmosphereCamera::default(),
+    )).insert(Name::new("camera"));
 }
