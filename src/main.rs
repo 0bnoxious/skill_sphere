ca79436abbccb5377c0308d001f2c26e1c7392f9
0bnoxious <n.beaudet234@gmail.com>
2023-07-06 19:16:04 -0400

    Added mesh materials

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,44 @@
 use bevy::{
+    math::Vec3A,
     prelude::*,
-    window::{PresentMode}, 
     render::{
-        mesh::{
-            Indices, PrimitiveTopology,
-            VertexAttributeValues, self,
-        },
+        mesh::{self, Indices, PrimitiveTopology, VertexAttributeValues},
         render_resource::{AsBindGroup, ShaderRef},
-    }, math::Vec3A,
+    },
+    window::PresentMode,
 };
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_atmosphere::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 pub const HEIGHT: f32 = 1080.0;
 pub const WIDTH: f32 = 1920.0;
 
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window {
-            title: "Skill Sphere".into(),
-            resolution: (WIDTH, HEIGHT).into(),
-            present_mode: PresentMode::AutoVsync,
-            // Tells wasm to resize the window according to the available canvas
-            fit_canvas_to_parent: true,
-            // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
-            prevent_default_event_handling: false,
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Skill Sphere".into(),
+                resolution: (WIDTH, HEIGHT).into(),
+                present_mode: PresentMode::AutoVsync,
+                // Tells wasm to resize the window according to the available canvas
+                fit_canvas_to_parent: true,
+                // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                ..default()
+            }),
             ..default()
-        }),
-        ..default()
-    }))
-    .add_plugin(AtmospherePlugin)
-    .add_startup_system(setup_scene)
-    .add_startup_system(setup_skillsphere)
-    .add_startup_system(setup_camera)
-    .add_plugin(WorldInspectorPlugin::new())
-    .run();
+        }))
+        .add_plugin(AtmospherePlugin)
+        .add_startup_system(setup_scene)
+        .add_startup_system(setup_skillsphere)
+        .add_startup_system(setup_camera)
+        .add_plugin(WorldInspectorPlugin::new())
+        .run();
 }
 
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
-pub struct SkillSphere {
-
-}
+pub struct SkillSphere {}
 
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
@@ -50,18 +46,18 @@ pub struct Pole {
     skill_name: String,
 }
 
-fn setup_scene(
-    mut commands: Commands,
-) {
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
+fn setup_scene(mut commands: Commands) {
+    commands
+        .spawn(PointLightBundle {
+            point_light: PointLight {
+                intensity: 1500.0,
+                shadows_enabled: true,
+                ..default()
+            },
+            transform: Transform::from_xyz(4.0, 8.0, 4.0),
             ..default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    }).insert(Name::new("Light"));
+        })
+        .insert(Name::new("Light"));
 
     commands.insert_resource(AtmosphereModel::new(Nishita {
         rayleigh_coefficient: Vec3::new(2e-5, 1e-5, 2e-5),
@@ -79,53 +75,41 @@ fn setup_skillsphere(
 
     let tetra_lenght: f32 = 1.0;
     /*let mut tetra_vertices = vec![];
-
-
-    tetra_vertices.push(Vec3::new(0.0, tetra_lenght * f32::sqrt(2./3.), 0.0)); //apex) 
+    tetra_vertices.push(Vec3::new(0.0, tetra_lenght * f32::sqrt(2./3.), 0.0)); //apex)
     tetra_vertices.push(Vec3::new(tetra_lenght / f32::sqrt(3.), 0.0, 0.0));
     tetra_vertices.push(Vec3::new(0.0, -(1. / (2. * f32::sqrt(3.))), tetra_lenght / 2.));
     tetra_vertices.push(Vec3::new(0.0, -(1. / (2. * f32::sqrt(3.))), -(tetra_lenght / 2.)));*/
 
     let tetra_vertices = vec![
-        Vec3::new(0.0, tetra_lenght * f32::sqrt(2./3.), 0.0), //apex
+        Vec3::new(0.0, tetra_lenght * f32::sqrt(2. / 3.), 0.0), //apex
         Vec3::new(tetra_lenght / f32::sqrt(3.), 0.0, 0.0),
-        Vec3::new(0.0, -(1. / (2. * f32::sqrt(3.))), tetra_lenght / 2.),
-        Vec3::new(0.0, -(1. / (2. * f32::sqrt(3.))), -(tetra_lenght / 2.))
+        Vec3::new(-(1. / (2. * f32::sqrt(3.))), 0.0, tetra_lenght / 2.),
+        Vec3::new(-(1. / (2. * f32::sqrt(3.))), 0.0, -(tetra_lenght / 2.)),
     ];
 
-   /* // Triangle side #1
-    tetra_indices.push(mesh::Indices::U32(vec![0, 2, 1]));
-    // Triangle side #2
-    tetra_indices.push(mesh::Indices::U32(vec![0, 2, 3]));
-    // Triangle side #3
-    tetra_indices.push(mesh::Indices::U32(vec![0, 3, 1]));
-    // Triangle #4 bottom
-    tetra_indices.push(mesh::Indices::U32(vec![1, 2, 3]));*/
-
+    // Normals
     let tetra_indices = Indices::U32(vec![
-        0, 2, 1,
-        0, 3, 2,
-        0, 3, 1,
-        1, 2, 3
+        0, 2, 1, 0, 3, 2, 0, 1, 3, 1, 2, 3, //bottom
     ]);
 
-    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION,tetra_vertices);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, tetra_vertices);
     // A triangle using vertices 0, 2, and 1.
     // Note: order matters. [0, 1, 2] will be flipped upside down, and you won't see it from behind!
     //mesh.set_indices(Some(mesh::Indices::U32(vec![0, 2, 1])));
     mesh.set_indices(Some(tetra_indices));
-
+    mesh.duplicate_vertices();
+    mesh.compute_flat_normals();
 
     //mesh.set_indices(Some(tetra_indices.into_iter().map(Some).collect()));
     //let as_option: Vec<Option<Indices>> = tetra_indices.into_iter().map(Some).collect();
     //mesh.set_indices(as_option.into_iter().collect());
 
     commands.spawn(PbrBundle {
-        mesh: meshes.add(mesh),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        mesh: meshes.add(mesh.clone()),
+        material: materials.add(Color::rgba(0.3, 0.5, 0.3, 1.0).into()),
         ..default()
     });
-    
+
     /*let skillsphere = commands.spawn(SceneBundle {
         scene: asset_server.load("Star.glb#Scene0"),
         transform: Transform::from_xyz(0.0, 0.0, 0.0),
@@ -138,22 +122,21 @@ fn setup_skillsphere(
         transform: Transform::from_xyz(0.0, 1.0, 0.0),
         ..default()
     }).insert(Pole{
-        skill_name: "Fire".to_string(), 
+        skill_name: "Fire".to_string(),
     }).insert(Name::new("Pole")).id();
 
     // add the child to the parent
     commands.entity(skillsphere).push_children(&[pole]);*/
 }
 
-fn setup_camera(
-    mut commands: Commands,
-) {
-    commands.spawn((
-        Camera3dBundle {
-            transform: Transform::from_xyz(-2.0, 1.0, 5.0)
-            .looking_at(Vec3::ZERO, Vec3::Y),
-            ..default()
-        },
-        AtmosphereCamera::default(),
-    )).insert(Name::new("camera"));
+fn setup_camera(mut commands: Commands) {
+    commands
+        .spawn((
+            Camera3dBundle {
+                transform: Transform::from_xyz(-2.0, 1.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+                ..default()
+            },
+            AtmosphereCamera::default(),
+        ))
+        .insert(Name::new("camera"));
 }
